e26f58b8768e26551c0779e1ebf351c5a62e4df8
babdus <babdusi@gmail.com>
2024-03-01 16:09:33 +0100

    Add new and edit to admin supergroups

--- app/controllers/admin/supergroups_controller.rb
+++ app/controllers/admin/supergroups_controller.rb
@@ -4,4 +4,30 @@ class Admin::SupergroupsController < Admin::BaseController
     @supergroups = Supergroup.all
   end
 
+  def new
+    @supergroup = Supergroup.new
+  end
+
+  def create
+    @supergroup = Supergroup.new(supergroup_params)
+    @supergroup.save!
+    redirect_to admin_supergroups_path
+  end
+
+  def edit
+    @supergroup = Supergroup.find(params[:id])
+  end
+
+  def update
+    @supergroup = Supergroup.update(supergroup_params)
+    @supergroup.save!
+    redirect_to admin_supergroups_path
+  end
+
+  private
+
+  def supergroup_params
+    params.require(:supergroup).permit(:name_ka, :name_en, :comment, :status)
+  end
+
 end
