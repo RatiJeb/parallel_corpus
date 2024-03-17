7d9fb6e7ec949094b46c844e0b5c4e9da905a0e8
babdus <babdusi@gmail.com>
2024-03-18 02:04:20 +0400

    Make design like designers wanted

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,18 @@
 module ApplicationHelper
 
+  @@main_color = 'sky'
+  @@secondary_color = 'slate'
+
   def header_link(name, url, mobile = false)
     if mobile
       if is_active_link?(url, :inclusive)
-        link_to(name, url, class: "border-indigo-500 bg-indigo-50 text-indigo-700 block border-l-4 py-2 pl-3 pr-4 text-base font-medium", 'aria-current': "page")
+        link_to(name, url, class: "border-skyblue-500 bg-skyblue-50 text-skyblue-700 block border-l-4 py-2 pl-3 pr-4 text-base font-medium", 'aria-current': "page")
       else
         link_to(name, url, class: "border-transparent text-gray-600 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800 block border-l-4 py-2 pl-3 pr-4 text-base font-medium")
       end
     else
       if is_active_link?(url, :inclusive)
-        link_to(name, url, class: "border-indigo-500 text-gray-900 inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium", 'aria-current': "page")
+        link_to(name, url, class: "border-skyblue-500 text-gray-900 inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium", 'aria-current': "page")
       else
         link_to(name, url, class: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium")
       end
@@ -45,29 +48,27 @@ module ApplicationHelper
   end
 
   def current_page(number, url_method, paging_params, records)
-    link_to(number, url_method.call(**paging_params.merge(page: records.current_page)), class: "relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", 'aria-current': "page")
+    link_to(number, url_method.call(**paging_params.merge(page: records.current_page)), class: "relative z-10 inline-flex items-center bg-skyblue-100 px-4 py-2 rounded-md text-sm font-semibold text-gray-900 focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-skyblue-200", 'aria-current': "page")
   end
 
   def other_page(number, url_method, paging_params, records)
-    link_to(number, url_method.call(**paging_params.merge(page: number)), class: "relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0")
+    link_to(number, url_method.call(**paging_params.merge(page: number)), class: "relative inline-flex items-center px-4 py-2 rounded-md text-sm font-semibold text-gray-900 hover:bg-gray-50 focus:z-20 focus:outline-offset-0")
   end
 
   def page_ellipsis
-    '<span class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0">...</span>'
+    '<span class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 focus:outline-offset-0">...</span>'
   end
 
   def table_header_cell_classes(column, index, size)
-    classes = "sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
-    classes += (index == 0) ? ' pl-4 sm:pl-6 lg:pl-8' : ' pl-3'
-    classes += (index == size - 1) ? ' pr-4 sm:pr-6 lg:pr-8' : ' pr-3'
+    classes = "sticky top-0 z-10 border-b border-r border-t border-gray-400 bg-skyblue-200 bg-opacity-75 py-3.5 px-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
+    classes += ' border-l' if index == 0
     classes += " #{column[:classes]}" if column[:classes]
     classes
   end
 
   def table_filter_cell_classes(column, index, size)
-    classes = "sticky top-12 z-11 border-b border-gray-300 bg-white bg-opacity-75 text-left text-sm text-gray-500 backdrop-blur backdrop-filter"
-    classes += (index == 0) ? ' pl-4 sm:pl-6 lg:pl-8' : ' pl-3'
-    classes += (index == size - 1) ? ' pr-4 sm:pr-6 lg:pr-8' : ' pr-3'
+    classes = "sticky top-12 z-11 border-b border-r border-gray-400 bg-neutral-200 bg-opacity-75 px-3 text-left text-sm text-gray-500 backdrop-blur backdrop-filter"
+    classes += ' border-l' if index == 0
     classes += " #{column[:classes]}" if column[:classes]
     classes
   end
@@ -84,10 +85,10 @@ module ApplicationHelper
     end
   end
 
-  def table_body_cell_classes(column, index, size)
-    classes = "whitespace-nowrap border-b border-gray-200 py-4 text-sm"
-    classes += (index == 0) ? ' pl-4 sm:pl-6 lg:pl-8' : ' pl-3'
-    classes += (index == size - 1) ? ' pr-4 sm:pr-6 lg:pr-8' : ' pr-3'
+  def table_body_cell_classes(column, index, size, row_index)
+    classes = "whitespace-nowrap border-b border-r py-4 px-3 text-sm"
+    classes += ' border-l' if index == 0
+    classes += " bg-slate-100" if row_index % 2 == 0
     classes += " #{column[:classes]}" if column[:classes]
     classes
   end
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -10,6 +10,26 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        skyblue: {
+          50: '#e5f1f4',
+          100: '#d5e8ed',
+          200: '#aad0db',
+          300: '#83bbcd',
+          400: '#5ba6bf',
+          500: '#3b96b4',
+          600: '#1385a8',
+          700: '#0d6985',
+          800: '#0c516b',
+          900: '#0a354a',
+          950: '#032333'
+        },
+        mountbatten: {
+          300: '#a6959d',
+          400: '#9e8892',
+          500: '#907c85'
+        }
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
