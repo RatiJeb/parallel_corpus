ed07c7232b6ac70466483a09b1b068c1fe805187
babdus <babdusi@gmail.com>
2024-03-20 02:15:28 +0400

    Change admin color to mountbatten

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,13 +3,13 @@ module ApplicationHelper
   def header_link(name, url, mobile = false)
     if mobile
       if is_active_link?(url, :inclusive)
-        link_to(name, url, class: "border-skyblue-500 bg-skyblue-50 text-skyblue-700 block border-l-4 py-2 pl-3 pr-4 text-base font-medium", 'aria-current': "page")
+        link_to(name, url, class: "border-mountbatten-500 bg-mountbatten-50 text-mountbatten-700 block border-l-4 py-2 pl-3 pr-4 text-base font-medium", 'aria-current': "page")
       else
         link_to(name, url, class: "border-transparent text-gray-600 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800 block border-l-4 py-2 pl-3 pr-4 text-base font-medium")
       end
     else
       if is_active_link?(url, :inclusive)
-        link_to(name, url, class: "border-skyblue-500 text-gray-900 inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium", 'aria-current': "page")
+        link_to(name, url, class: "border-mountbatten-500 text-gray-900 inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium", 'aria-current': "page")
       else
         link_to(name, url, class: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium")
       end
@@ -45,11 +45,11 @@ module ApplicationHelper
   end
 
   def current_page(number, url_method, paging_params, records)
-    link_to(number, url_method.call(**paging_params.merge(page: records.current_page)), class: "relative z-10 inline-flex items-center bg-skyblue-100 px-4 py-2 rounded-md text-sm font-semibold text-gray-900 focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-skyblue-200", 'aria-current': "page")
+    link_to(number, url_method.call(**paging_params.merge(page: records.current_page)), class: "relative z-10 inline-flex items-center bg-mountbatten-100 px-4 py-2 rounded-md text-sm font-semibold text-gray-900 focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-mountbatten-200", 'aria-current': "page")
   end
 
   def other_page(number, url_method, paging_params, records)
-    link_to(number, url_method.call(**paging_params.merge(page: number)), class: "relative inline-flex items-center px-4 py-2 rounded-md text-sm font-semibold text-gray-900 hover:bg-gray-50 focus:z-20 focus:outline-offset-0")
+    link_to(number, url_method.call(**paging_params.merge(page: number)), class: "relative inline-flex items-center px-4 py-2 rounded-md text-sm font-semibold text-gray-900 hover:bg-mountbatten-50 focus:z-20 focus:outline-offset-0")
   end
 
   def page_ellipsis
@@ -57,14 +57,15 @@ module ApplicationHelper
   end
 
   def table_header_cell_classes(column, index, size)
-    classes = "sticky top-0 z-10 border-b border-r border-t border-gray-400 bg-skyblue-200 bg-opacity-75 py-3.5 px-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
+    classes = "sticky top-0 z-10 border-b border-r border-t border-gray-400 bg-mountbatten-200 bg-opacity-75 py-3.5 px-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
     classes += ' border-l' if index == 0
     classes += " #{column[:classes]}" if column[:classes]
     classes
   end
 
   def table_filter_cell_classes(column, index, size)
-    classes = "sticky top-12 z-11 border-b border-r border-gray-400 bg-neutral-200 bg-opacity-75 px-2 text-left text-sm text-gray-500 backdrop-blur backdrop-filter"
+    classes = "sticky top-12 z-11 border-b border-r border-gray-400 bg-gray-200 bg-opacity-75 text-left text-sm text-gray-500 backdrop-blur backdrop-filter"
+    classes += ([:text_field, :status_select, :submit].include?(column[:input_type])) ? ' px-2' : ' px-3'
     classes += ' border-l' if index == 0
     classes += " #{column[:classes]}" if column[:classes]
     classes
@@ -72,11 +73,11 @@ module ApplicationHelper
 
   def table_filter_input(form, column)
     if column[:input_type] == :text_field
-      classes = "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-3xl focus:ring-blue-500 focus:border-blue-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      classes = "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-3xl focus:ring-oceanside-500 focus:border-oceanside-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-oceanside-500 dark:focus:border-oceanside-500"
       classes += " #{column[:input_classes]}" if column[:input_classes]
       form.text_field column[:name], name: column[:name], class: classes
     elsif column[:input_type] == :status_select
-      form.select :status, column[:model].statuses.keys.map { |status| [status.humanize, status] }, { include_blank: true }, { name: :status, class: "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-3xl focus:ring-blue-500 focus:border-blue-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" }
+      form.select :status, column[:model].statuses.keys.map { |status| [status.humanize, status] }, { include_blank: true }, { name: :status, class: "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-3xl focus:ring-oceanside-500 focus:border-oceanside-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-oceanside-500 dark:focus:border-oceanside-500" }
     elsif column[:input_type] == :hidden
       form.hidden_field column[:name], name: column[:name]
     else
@@ -87,14 +88,14 @@ module ApplicationHelper
   def table_body_cell_classes(column, index, size, row_index)
     classes = "whitespace-nowrap border-b border-r py-4 px-3 text-sm"
     classes += ' border-l' if index == 0
-    classes += " bg-slate-100" if row_index % 2 == 0
+    classes += " bg-slate-50" if row_index % 2 == 0
     classes += " #{column[:classes]}" if column[:classes]
     classes
   end
 
   def table_body_cell_contents(record, column)
     if column[:type] == :link
-      link_to((record.respond_to?(column[:name]) ? record.send(column[:name]) : column[:name]), column[:url_method].call(**column[:url_params]), class: "text-skyblue-700 hover:text-skyblue-900")
+      link_to((record.respond_to?(column[:name]) ? record.send(column[:name]) : column[:name]), column[:url_method].call(**column[:url_params]), class: "text-gray-700 hover:text-gray-900")
     else
       record.send(column[:name])
     end
