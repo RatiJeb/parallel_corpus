0e3deb978a2db3fc573b8df90fcef500b7b8ef1c
babdus <babdusi@gmail.com>
2024-05-05 03:28:36 +0400

    Make tooltips for editor actions

--- app/javascript/controllers/tooltip_controller.js
+++ app/javascript/controllers/tooltip_controller.js
@@ -2,10 +2,14 @@ import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
   static targets = ['tooltip', 'cell']
+  static values = {
+    overflow: Boolean,
+    placement: String
+  }
 
   connect() {
     this.cellTarget.addEventListener("mouseenter", () => {
-      if (this.isOverflowing()) {
+      if (!this.overflowValue || this.isOverflowing()) {
         this.showTooltip()
       }
     })
@@ -28,11 +32,27 @@ export default class extends Controller {
 
     const rect = this.cellTarget.getBoundingClientRect()
     const scrollTop = window.pageYOffset || document.documentElement.scrollTop
-    const left = rect.left + window.pageXOffset
-    const top = rect.bottom + scrollTop
 
-    this.tooltip.style.left = `${left}px`;
-    this.tooltip.style.top = `${top-10}px`;
+    if (!this.placementValue || this.placementValue == 'bottom') {
+
+      const left = Math.max(rect.left + window.pageXOffset - this.tooltip.offsetWidth/2 + rect.width/2, 0)
+      const top = rect.bottom + scrollTop + 2
+
+      this.tooltip.style.left = `${left}px`;
+      this.tooltip.style.top = `${top}px`;
+
+    } else if (this.placementValue == 'top') {
+
+      const left = Math.max(rect.left + window.pageXOffset - this.tooltip.offsetWidth/2 + rect.width/2, 0)
+      const top = Math.max(rect.top + scrollTop - this.tooltip.offsetHeight - 5)
+
+      console.log(left)
+      console.log(top)
+
+      this.tooltip.style.left = `${left}px`;
+      this.tooltip.style.top = `${top}px`;
+
+    }
   }
 
   hideTooltip() {
