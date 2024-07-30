a4baa94fc23b11f61f6b181d7c4903a9633a93e3
Rati Jebisashvili <85803535+RatiJeb@users.noreply.github.com>
2024-07-30 13:12:37 +0400

    origin/8-change-default-search-to-look-for-every-match (#19)
    before: 
    <img width="342" alt="image"
    src="https://github.com/user-attachments/assets/b05376ca-52cb-4c22-80b7-c4fb6137e093">
    after: 
    <img width="342" alt="image"
    src="https://github.com/user-attachments/assets/225f22a6-0f11-42c8-b009-c16ad8bb77e5">

--- app/javascript/controllers/search_highlight_controller.js
+++ app/javascript/controllers/search_highlight_controller.js
@@ -3,15 +3,30 @@ import { Controller } from '@hotwired/stimulus'
 export default class extends Controller {
   static targets = ['searchbar', 'body']
   static values = {
-    fullWordMatch: String
+    fullWordMatch: String,
+    exactMatch: Boolean
   }
 
   connect() {
+    this.highlight();
+  }
 
+  highlight() {
     this.searchbarTarget.querySelectorAll("input[type=text]").forEach((text_field) => {
       this.bodyTarget.querySelectorAll(".highlightable").forEach((a) => {
         if (this.fullWordMatchValue) {
-          if (text_field.value && a.textContent.search(new RegExp('(^|[^a-zA-Z0-9ა-ჰ-])(' + text_field.value + ')($|[^a-zA-Z0-9ა-ჰ-])', 'i')) >= 0) {
+          if (!this.exactMatchValue) {
+            if (text_field.value && a.textContent.search(new RegExp( '(' + text_field.value + ')', 'gi')) >= 0) {
+              var parts = a.textContent.split(new RegExp('(' + text_field.value + ')', 'gi'))
+              for (var i = 0; i < parts.length; i++) {
+                if (parts[i] === text_field.value) {
+                  parts[i] = '<span class="text-magenta-500 bg-yellow-100">' + parts[i] + '</span>'
+                }
+              }
+              a.innerHTML = parts.join('')
+            }
+          }
+          else if (text_field.value && a.textContent.search(new RegExp('(^|[^a-zA-Z0-9ა-ჰ-])(' + text_field.value + ')($|[^a-zA-Z0-9ა-ჰ-])', 'i')) >= 0) {
             var parts = a.textContent.split(new RegExp('(^|[^a-zA-Z0-9ა-ჰ-])(' + text_field.value + ')($|[^a-zA-Z0-9ა-ჰ-])', 'gi'))
             for (var i = 0; i < parts.length; i++) {
               if (i % 4 == 2) {
@@ -35,5 +50,10 @@ export default class extends Controller {
       })
     })
 
+  }
+
+  toggleExactMatch(event) {
+    this.exactMatchValue = event.target.checked;
+    this.highlight();
   }
 }
