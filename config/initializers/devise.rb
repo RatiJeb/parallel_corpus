b1ce0e96d1008598528df03d62e2473c4416d710
Rati Jebisashvili <rjebisashvili@gmail.com>
2025-02-20 08:37:42 +0400

    add password change mailer config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,13 +24,13 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  # config.mailer_sender = ''
+  config.mailer_sender = 'noreply-pcor@ses.iliauni.edu.ge'
 
   # Configure the class responsible to send e-mails.
-  config.mailer = 'ApplicationMailer'
+  config.mailer = 'Devise::Mailer'
 
   # Configure the parent class responsible to send e-mails.
-  # config.parent_mailer = 'ActionMailer::Base'
+  config.parent_mailer = 'ActionMailer::Base'
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
